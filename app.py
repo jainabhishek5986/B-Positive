f71a91d9184b984d9d0ceb70dfb094ae25c0cdde
jainabhishek5986 <abhishekjain.28031997@gmail.com>
2018-06-26 20:12:19 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify,render_template
 from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////home/shivam/asdf/try1.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////home/abhishek/Blood/B-Positive/database'.db'
 db=SQLAlchemy(app)
 
 
@@ -11,11 +11,7 @@ def hello():
 @app.route("/product/<int:a>/",methods=['GET','POST'])
 def pro(a):
     if request.method=='GET':
-        entry = Product(name='anshul',blood_group='B+',address='',city='Pune',how_often=10,contact=5839445932,email_id='anshul75222@gmail.com')
-        db.session.add(entry)
-        db.session.commit()
-        return jsonify({'name':'shivam','blood_group':'AB+','address':'','city':'Pune','how_often':'5','contact':'9454894755','email_id':'xshivampathak75222@gmail.com'})
-
+        return "Hello"
 
 class Product(db.Model):
     name=db.Column(db.String(100),nullable=False)
